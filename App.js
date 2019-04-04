6fc8a73aad683b1d514c1880afff9a05a144fcc3
austin <austin@austinui-MacBookPro.local>
2019-04-04 12:52:07 +0900

    admob 추가

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, ActivityIndicator, StatusBar } from 'react-native';
 import Weather from "./components/WeatherIndex";
+import { AdMobBanner } from 'expo';
 
 export default class App extends React.Component {
+  bannerError(e) {
+    alert(e);
+  }
+
   state = {
     isLoaded: true
   }
@@ -11,13 +16,18 @@ export default class App extends React.Component {
     
     return (
       <View style={styles.container}>
-      <StatusBar hidden={ true } />
+        <StatusBar hidden={ true } />
         {isLoaded ? <Weather /> : (
           <View style={styles.loading}>
             <ActivityIndicator size="large"/>
             <Text style={styles.textLoading}>Getting Data from Server...</Text>
           </View>
         )}
+        <AdMobBanner 
+          bannerSize="smartBannerLandscape"
+          adUnitID="ca-app-pub-7650830264106685/3543064850"
+          onDidFailToReceiveAdWithError={ (e) => this.bannerError(e)}
+        />
       </View>
     );
   }
